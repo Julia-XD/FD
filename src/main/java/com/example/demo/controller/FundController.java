e9d20ebf851e7bed8346cc0a731b42b30f5e963a
Xiaohan <120512900@qq.com>
2021-08-12 03:16:25 +0000

    fund crud

--- src/main/java/com/example/demo/controller/FundController.java
+++ src/main/java/com/example/demo/controller/FundController.java
@@ -1,17 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Fund;
+import com.example.demo.entity.FundManager;
 import com.example.demo.service.FundManagerService;
 import com.example.demo.service.FundService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/fund")
-@Api(value = "Fund CRUD" ,tags = {"FundController"})
+@Api(value = "Fund CRUD", tags = {"FundController"})
 public class FundController {
     @Autowired
     private FundService fundService;
@@ -19,20 +21,116 @@ public class FundController {
     @Autowired
     private FundManagerService fundManagerService;
 
-    @ApiOperation(value = "Add a fund ",notes = "Add a fund Api")
-    @RequestMapping(value = "/post",method = RequestMethod.POST)
-    public boolean add(int employeeId,String name){
+
+    //查询
+    @GetMapping("/list")
+    public List<Fund> list() {
+        return fundService.list();
+    }
+
+    @GetMapping("/list-name/{name}")
+    public List<Fund> list1(@PathVariable String name) {
+        return fundService.list1(name);
+    }
+
+    @GetMapping("/list-employeeid/{id}")
+    public List<Fund> list2(@PathVariable int id) {
+        return fundService.list2(id);
+    }
+
+    @GetMapping("/list-fundid/{id}")
+    public List<Fund> list3(@PathVariable int id) {
+        return fundService.list3(id);
+    }
+
+    //增加
+    @ApiOperation(value = "Add a fund ", notes = "Add a fund Api")
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public boolean add(int employeeId, String name) {
         //先判断employeeid存在吗
-        if(!fundManagerService.isFundManager(employeeId)) {
+        if (!fundManagerService.isFundManager(employeeId)) {
             try {
                 throw new Exception();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return false;
-        }else {
-            boolean flag = fundService.add(employeeId,name);
+        } else {
+            boolean flag = fundService.add(employeeId, name);
             return flag;
         }
     }
-}
+
+    //@PostMapping("/save")
+    //    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+    //        CommonResp resp = new CommonResp<>();
+    //        docService.save(req);
+    //        return resp;
+    //    }
+    @RequestMapping(value = "/post-json", method = RequestMethod.POST)
+    public boolean add1(@RequestBody Fund fund) {
+        if (!fundManagerService.isFundManager(fund.getEmployeeId())) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        } else {
+            boolean flag = fundService.add(fund.getEmployeeId(),fund.getName());
+            return flag;
+        }
+
+    }
+
+
+
+        //修改
+        @RequestMapping(value = "/update", method = RequestMethod.PUT)
+        public boolean update ( int fundId, int employeeId, String name){
+            //先判断fundid存在吗
+            //先判断employeeid存在吗
+            if (!fundService.isFund(fundId)) {
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            } else {
+                if (!fundManagerService.isFundManager(employeeId)) {
+                    try {
+                        throw new Exception();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                } else {
+                    boolean flag = fundService.update(fundId, employeeId, name);
+                    return flag;
+                }
+            }
+        }
+
+        //删除
+        @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+        public boolean delete ( int fundId){
+            //先判断fundid存在吗
+            //先判断employeeid存在吗
+            if (!fundService.isFund(fundId)) {
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            } else {
+
+                boolean flag = fundService.delete(fundId);
+                return flag;
+
+            }
+        }
+    }
+
+
--- src/main/java/com/example/demo/service/FundService.java
+++ src/main/java/com/example/demo/service/FundService.java
@@ -1,15 +1,48 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Fund;
+import com.example.demo.entity.FundExample;
 import com.example.demo.entity.FundManager;
+import com.example.demo.entity.FundManagerExample;
 import com.example.demo.mapper.FundMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FundService {
+
     @Autowired
     private FundMapper fundMapper;
+
+    public List<Fund> list(){
+        return fundMapper.selectByExample(null);
+    }
+
+    public List<Fund> list1(String name){
+
+        FundExample fundExample = new FundExample();
+        FundExample.Criteria criteria = fundExample.createCriteria();
+        criteria.andNameLike("%"+name+"%");
+        return fundMapper.selectByExample(fundExample);
+    }
+
+    public List<Fund> list2(int id ){
+        FundExample fundExample = new FundExample();
+        FundExample.Criteria criteria = fundExample.createCriteria();
+        criteria.andEmployeeIdEqualTo(id);
+        return fundMapper.selectByExample(fundExample);
+    }
+
+    public List<Fund> list3(int id ){
+        FundExample fundExample = new FundExample();
+        FundExample.Criteria criteria = fundExample.createCriteria();
+        criteria.andFundIdEqualTo(id);
+        return fundMapper.selectByExample(fundExample);
+    }
+
+
     public boolean add(int id,String name){
         Fund fund = new Fund();
         fund.setEmployeeId(id);
@@ -22,4 +55,23 @@ public class FundService {
         Fund fund = fundMapper.selectByPrimaryKey(id);
         return fund!=null;
     }
+
+    public boolean update(int fundId,int employeeId,String name){
+        Fund fund = new Fund();
+        fund.setFundId(fundId);
+        fund.setEmployeeId(employeeId);
+        fund.setName(name);
+
+        int flag = fundMapper.updateByPrimaryKey(fund);
+
+        return flag==1;
+    }
+    public boolean delete(int fundId){
+
+
+        int flag = fundMapper.deleteByPrimaryKey(fundId);
+
+        return flag==1;
+    }
+
 }
